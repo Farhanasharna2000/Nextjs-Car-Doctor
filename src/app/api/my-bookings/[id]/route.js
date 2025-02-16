edb824dfe88bcc279b67538b161f0f06472355f9
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-02-17 01:03:18 +0600

    complete edit individual booking data functionality

---
+++ src/app/api/my-bookings/[id]/route.js
@@ -0,0 +1,28 @@
+import dbConnect, { collectionNamesObj } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+
+export const GET=async(req,{params})=>{
+    const p = await params;
+    const bookingsCollection = dbConnect(collectionNamesObj.bookingsCollection)
+    const query={ _id: new ObjectId(p.id) }
+    const singleBooking = await bookingsCollection.findOne(query)
+    return NextResponse.json(singleBooking) 
+}
+
+export const PATCH =async(req,{params})=>{
+    const p = await params;
+    const bookingsCollection = dbConnect(collectionNamesObj.bookingsCollection)
+    const query={ _id: new ObjectId(p.id) }
+    const body=await req.json()
+    const filter={
+        $set:{...body}
+    }
+    const option={
+        upsert:true
+    }
+    const updateResponse = await bookingsCollection.updateOne(query,filter,option)
+    revalidatePath('my-bookings')
+    return NextResponse.json(updateResponse) 
+}
