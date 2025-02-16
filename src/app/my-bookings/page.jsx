9eea6c249bd0997b7568fc22b9db9730d3bffc84
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-02-16 22:25:00 +0600

    show bookings in my bookings page

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -1,10 +1,20 @@
-import MyAllBookings from '@/components/tables/MyBookinsTable';
-import React from 'react';
+'use client'
+import MyAllBookingsTable from '@/components/tables/MyBookinsTable';
+import React, { useEffect, useState } from 'react';
 
 const MyBookingsPage = () => {
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        const fetchMyBookings=async()=>{
+const res=await fetch('http://localhost:3000/api/service')
+const d=await res.json()
+setData(d)
+        }
+        fetchMyBookings()
+    },[])
     return (
         <div>
-            <MyAllBookings/>
+            <MyAllBookingsTable data={data}/>
         </div>
     );
 };
